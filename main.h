2f89c970a3d304f4caaa411d59a138b8a149d19b
WambuBObo <99308585+WambuBObo@users.noreply.github.com>
2022-04-23 05:27:21 +0300

    Update main.h

--- main.h
+++ main.h
@@ -28,5 +28,6 @@ int print_p(va_list p);
 int print_S(va_list S);
 int print_r(va_list r);
 int print_R(va_list R);
-
+void print_binary(unsigned int n, unsigned int* printed);
+int print_unsignedToBinary(va_list arg);
 #endif
