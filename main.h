a1f8fcd8445e410da3de4eae81fc90ef1f90edec
WambuBObo <99308585+WambuBObo@users.noreply.github.com>
2022-04-23 04:31:33 +0300

    Update main.h

--- main.h
+++ main.h
@@ -1,62 +1,33 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef MAIN.H
+#define MAIN.H
 
 #include <stdarg.h>
-#include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <limits.h>
-#include <string.h>
+
+int _putchar(char c);
+int print_int(va_list arg);
+int print_unsigned(va_list arg);
+int _printf(const char *format, ...);
+int print_char(va_list arg);
+int print_str(va_list arg);
+int print_percent(void);
+void print_binary(unsigned int n, unsigned int* printed);
+int print_unsignedToBinary(va_list arg);
+int print_oct(va_list arg);
+int print_unsignedIntToHex(unsigned int num, char _case);
+int print_hex_base(va_list arg, char _case);
+int print_hex(va_list arg);
+int print_HEX(va_list arg);
+int print_STR (va_list arg);
 
 /**
- * struct func_type - type structure
- * @t: pointer to the argument
- * @f: pointer-function associated with the argument
+ * struct identifierStruct - structure definition of a printTypeStruct
+ * @indentifier: type
+ * @printer: function to print
  */
-typedef struct func_type
+typedef struct identifierStruct
 {
-	char *t;
-	int (*f)(va_list);
-} func_t;
+char *indentifier;
+int (*printer)(va_list);
+} identifierStruct;
 
-int (*caller(const char *format))(va_list);
-int _putchar(char c);
-int _printf(const char *format, ...);
-int print_str(va_list args);
-int print_char(va_list args);
-int print_pct(va_list args);
-int print_dec(va_list args);
-int print_bin(va_list args);
-int print_int(va_list args);
-int print_bnr(va_list args);
-int print_unt(va_list args);
-int print_oct(va_list args);
-int print_hex(va_list args);
-int print_upx(va_list args);
-int print_usr(va_list args);
-int print_add(va_list args);
-int print_rev(va_list args);
-int print_rot(va_list args);
-int prinlint(va_list args);
-int prinlunt(va_list args);
-int prinloct(va_list args);
-int prinlhex(va_list args);
-int prinlupx(va_list args);
-int prinhint(va_list args);
-int prinhunt(va_list args);
-int prinhoct(va_list args);
-int prinhhex(va_list args);
-int prinhupx(va_list args);
-int prinpint(va_list args);
-int prinnoct(va_list args);
-int prinnhex(va_list args);
-int prinnupx(va_list args);
-int prinsint(va_list args);
-int (*get_print_func(const char *s, int index))(va_list, char *, unsigned int);
-int ev_print_func(const char *s, int index);
-char *fill_binary_array(char *binary, long int int_in, int isneg, int limit);
-char *fill_oct_array(char *bnr, char *oct);
-char *fill_long_oct_array(char *bnr, char *oct);
-char *fill_short_oct_array(char *bnr, char *oct);
-char *fill_hex_array(char *bnr, char *hex, int isupp, int limit);
 #endif
